ceb67c0406d2b1d42e6e02da592c2f761c875168
Randy Hunter <randyh252@gmail.com>
2022-08-08 21:32:40 -0400

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>© 2021 Hunter development. All rights reserved.</small>
+    </footer>
+  );
+}
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img className="nav-logo" src="./logo192.png" alt="logo" />
+        <ul className="nav-items">
+          <li>
+            <a href="https://github.com/rhunter252">Pricing</a>
+          </li>
+          <li>
+            <a href="https://github.com/rhunter252">About</a>
+          </li>
+          <li>
+            <a href="https://github.com/rhunter252">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
